be0416857c1dc32689a0d26ceaf819efe1950ac8
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-09-24 21:09:35 +0300

    second commit

--- lab-4.5/lab-4.5.cpp
+++ lab-4.5/lab-4.5.cpp
@@ -15,6 +15,23 @@ int main()
     cout << "R1 = "; cin >> R1;
     cout << "R2 = "; cin >> R2;
 
+    for (int i = 0; i < 10; i++) {
+            int x, y;
+            cout << "Ââåä³òü X ³ Y: ";
+            cin >> x >> y;
+
+            double D = sqrt(x * x + y * y);
+
+            if (D > R1 && D <= R2) {
+                cout << setw(8) << setprecision(4) << x << " "
+                    << setw(8) << setprecision(4) << y << " " << "yes" << endl;
+            }
+            else {
+                cout << setw(8) << setprecision(4) << x << " "
+                    << setw(8) << setprecision(4) << y << " " << "no" << endl;
+            }
+    }
+
     for (int i = 0; i < 10; i++)
     {
         x = rand() % (2 * R1 + 1) - R1;
